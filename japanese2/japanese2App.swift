340cc24ecdc07294c8591f7725264c41357b8dcc
chenzhizs <chenzhi@qq.com>
2021-04-27 10:58:17 +0900

    Initial Commit

---
+++ japanese2/japanese2App.swift
@@ -0,0 +1,17 @@
+//
+//  japanese2App.swift
+//  japanese2
+//
+//  Created by chenzhizs on 2021/04/27.
+//
+
+import SwiftUI
+
+@main
+struct japanese2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
